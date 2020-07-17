8e85a604375d751eac091139ee00d63e749e88e5
ak6130 <albert.polletta@gmail.com>
2020-07-17 16:29:29 +0200

    Update home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -8,7 +8,7 @@ include("./inc/header.inc"); ?>
 		include ("./inc/carousel.inc"); ?>
 			<div class="card-group">
 			  <div class="card">
-			    <img src="..." class="card-img-top" alt="...">
+			    <img src="<?=$page->carousel_img_1->url?>" class="card-img-top" alt="...">
 			    <div class="card-body">
 			      <h5 class="card-title">Card title</h5>
 			      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -16,7 +16,7 @@ include("./inc/header.inc"); ?>
 			    </div>
 			  </div>
 			  <div class="card">
-			    <img src="..." class="card-img-top" alt="...">
+			    <img src="<?=$page->carousel_img_2->url?>" class="card-img-top" alt="...">
 			    <div class="card-body">
 			      <h5 class="card-title">Card title</h5>
 			      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -24,7 +24,7 @@ include("./inc/header.inc"); ?>
 			    </div>
 			  </div>
 			  <div class="card">
-			    <img src="..." class="card-img-top" alt="...">
+			    <img src="<?=$page->carousel_img_3->url?>" class="card-img-top" alt="...">
 			    <div class="card-body">
 			      <h5 class="card-title">Card title</h5>
 			      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>

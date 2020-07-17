b991f5ccdff9d94a737a97245d18e4dd302024a3
ak6130 <albert.polletta@gmail.com>
2020-07-17 16:32:47 +0200

    Update home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -10,15 +10,15 @@ include("./inc/header.inc"); ?>
 			  <div class="card">
 			    <img src="<?=$page->carousel_img_1->url?>" class="card-img-top" alt="...">
 			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			      <h5 class="card-title">Aus der Masse abheben</h5>
+			      <p class="card-text"></p>
 			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 			    </div>
 			  </div>
 			  <div class="card">
 			    <img src="<?=$page->carousel_img_2->url?>" class="card-img-top" alt="...">
 			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
+			      <h5 class="card-title">Den grauen Alltag rot färben</h5>
 			      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
 			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 			    </div>
@@ -26,7 +26,7 @@ include("./inc/header.inc"); ?>
 			  <div class="card">
 			    <img src="<?=$page->carousel_img_3->url?>" class="card-img-top" alt="...">
 			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
+			      <h5 class="card-title">Besondere Details entdecken</h5>
 			      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
 			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 			    </div>

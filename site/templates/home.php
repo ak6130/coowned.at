ea2e58cfdb7c6e89ecb876f45293ca7cc11b2475
ak6130 <albert.polletta@gmail.com>
2020-07-17 12:55:43 +0200

    update filestructure
    include files added
    basic page deleted
    home.php update

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,3 +1,10 @@
 <?php
 
-include("./basic-page.php"); 
+include("./inc/header.inc"); ?>
+
+	<body>
+		<h1><?php echo $page->title; ?></h1>
+		<?php if($page->editable()) echo "<p><a href='$page->editURL'>Edit</a></p>"; ?>
+	
+	</body>
+</html>

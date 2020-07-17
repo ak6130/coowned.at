19a83a3bf093d3aeac6d601ebb3a368077f109e3
ak6130 <albert.polletta@gmail.com>
2020-07-17 16:15:31 +0200

    Update home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -4,7 +4,7 @@ include("./inc/header.inc");
 include ("./inc/navbar.inc");?>
 
 	<body>
-		<div class="container-fluid">
+		<div class="container-fluid" max-height="400">
 			<?php include ("./inc/carousel.inc"); ?>
 		</div>
 
